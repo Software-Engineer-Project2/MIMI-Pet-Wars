a1f023ea8b4af6e4e3e4411a1b1ec1927659f3f3
e <Viola_futong@163.com>
2020-03-16 23:19:07 +0800

    submit backend modification

--- hospitalapp/config.py
+++ hospitalapp/config.py
@@ -1,9 +1,8 @@
 import os
-
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_COMMIT_TEARDOWN = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
